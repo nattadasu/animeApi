4e1e317c69657ecbdf6627d02f9346d90b7fc7c5
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-10-14 09:00:37 +0700

    Add auto redirect support

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
 """Main API file"""
 
-#pylint: disable=import-error
+# pylint: disable=import-error
 
 import json
 from datetime import datetime
-from urllib.parse import unquote
 from time import time
 from typing import Any, Union
+from urllib.parse import unquote
 
-from flask import (g, jsonify, Flask, send_from_directory,  # type: ignore
-                   Response, request, redirect)
+from flask import send_from_directory  # type: ignore
+from flask import Flask, Response, g, jsonify, redirect, request
 
 app = Flask(__name__)
 
@@ -165,16 +165,15 @@ def trakt_exclusive_route(media_type: str, media_id: int, season_id: Union[str, 
 def tmdb_exclusive_route(media_type: str, media_id: int, season_id: Union[str, None] = None):
     """
     The Movie Database exclusive route
-    
+
     Args:
         media_type (str): Media type, must be `movie`
         media_id (int): Media ID
         season_id (Union[str, None], optional): Season ID. Defaults to None.
-    
+
     Returns:
         Response: Response
     """
-    media_type_ = media_type
     if media_type == "tv" or season_id is not None:
         return jsonify({
             "error": "Invalid request",
@@ -182,8 +181,6 @@ def tmdb_exclusive_route(media_type: str, media_id: int, season_id: Union[str, N
             "message": "Currently, only `movie` are supported"
         }), 400
     try:
-        if media_type.endswith("s"):
-            media_type_ = media_type[:-1]
         return platform_id_content("themoviedb", f"movie/{media_id}")
     except KeyError:
         return jsonify({
@@ -210,7 +207,8 @@ def platform_array(platform: str = "animeapi"):
     platform = platform.lower()
     if route.endswith(".tsv"):
         with open("database/animeapi.tsv", "r", encoding="utf-8") as file_:
-            response = Response(file_.read(), mimetype="text/tab-separated-values", status=200)
+            response = Response(
+                file_.read(), mimetype="text/tab-separated-values", status=200)
             response.headers['Content-Disposition'] = 'inline; filename="animeapi.tsv"'
             return response
     # remove .json if present
@@ -224,6 +222,7 @@ def platform_array(platform: str = "animeapi"):
         platform = "shoboi"
     return redirect(
         f"https://raw.githubusercontent.com/nattadasu/animeApi/v3/database/{platform}.json")
+
 
 @app.route("/<platform>/<platform_id>", methods=["GET"])
 def platform_lookup(platform: str, platform_id: Union[int, str]):
@@ -256,8 +255,164 @@ def platform_lookup(platform: str, platform_id: Union[int, str]):
             "message": f"Platform {platform} with ID {platform_id} not found"
         }), 404
 
+# redirect route
+# example: /rd?platform=anilist&platform_id=1&to=kitsu
+
+
+@app.route("/rd", methods=["GET"])
+@app.route("/redirect", methods=["GET"])
+def redirect_route():
+    """
+    Redirect route
+
+    Returns:
+        Response: Redirect response
+    """
+    args = request.args
+    # get platform and platform ID
+    platform = args.get("platform") or args.get("from")
+    platform = str(platform).lower()
+    platform_id = args.get("mediaid") or args.get("id")
+    target = args.get("target") or args.get("to")
+    israw = args.get("raw") or args.get("r")
+    # convert to switch bool whatever the value is in raw if exist
+    israw = israw is not None
+    route_path = {
+        "anidb": "https://anidb.net/anime/",
+        "anilist": "https://anilist.co/anime/",
+        "animeplanet": "https://www.anime-planet.com/anime/",
+        "anisearch": "https://www.anisearch.com/anime/",
+        "annict": "https://annict.com/works/",
+        "imdb": "https://www.imdb.com/title/",
+        "kaize": "https://kaize.io/anime/",
+        "kitsu": "https://kitsu.io/anime/",
+        "livechart": "https://www.livechart.me/anime/",
+        "myanimelist": "https://myanimelist.net/anime/",
+        "nautiljon": "https://www.nautiljon.com/animes/",
+        "notify": "https://notify.moe/anime/",
+        "otakotaku": "https://otakotaku.com/anime/view/",
+        "shikimori": "https://shikimori.me/animes/",
+        "shoboi": "https://cal.syoboi.jp/tid/",
+        "silveryasha": "https://db.silveryasha.web.id/anime/",
+        "themoviedb": "https://www.themoviedb.org/movie/",
+        "trakt": "https://trakt.tv/",
+        "simkl": "https://simkl.com/"
+    }
+    # handle synonyms
+    synonyms = {
+        "anidb":       ["anidb", "adb", "anidb.net"],
+        "anilist":     ["anilist", "al", "anilist.co"],
+        "animeplanet": ["animeplanet", "ap", "anime-planet", "anime-planet.com"],
+        "anisearch":   ["anisearch", "as", "anisearch.com", "anisearch.de",
+                        "anisearch.fr", "anisearch.jp", "anisearch.es",
+                        "anisearch.it"],
+        "annict":      ["annict", "anc", "act", "ac", "annict.com", "annict.jp",
+                        "en.annict.com"],
+        "imdb":        ["imdb", "imdb.com"],
+        "kaize":       ["kaize", "kz", "kaize.io"],
+        "kitsu":       ["kitsu", "kts", "kt", "kitsu.io"],
+        "livechart":   ["livechart", "lc", "livechart.me"],
+        "myanimelist": ["myanimelist", "mal", "myanimelist.net"],
+        "nautiljon":   ["nautiljon", "ntj", "nautiljon.com"],
+        "notify":      ["notify", "ntf", "ntm", "nm", "nf", "notifymoe",
+                        "notify.moe"],
+        "otakotaku":   ["otakotaku", "oo", "otakotaku.com"],
+        "simkl":       ["simkl", "smk", "simkl.com"],
+        "shikimori":   ["shikimori", "shiki", "shk", "shikimori.org",
+                        "shikimori.one", "shikimori.me"],
+        "shoboi":      ["shoboi", "shobocal", "syoboi", "syobocal", "syb", "shb",
+                        "sb", "cal.syoboi.jp"],
+        "silveryasha": ["silveryasha", "dbti", "sy", "db.silveryasha.web.id"],
+        "themoviedb":  ["themoviedb", "tmdb", "tmdb.org"],
+        "trakt":       ["trakt", "trk", "trakt.tv"],
+    }
+
+    pf_check = False
+    tg_check = False if target else True
+    for key, value in synonyms.items():
+        if not pf_check:
+            if platform in value:
+                pf_check = True
+                platform = key
+        if not tg_check:
+            if target in value:
+                tg_check = True
+                target = key
+        if pf_check and tg_check:
+            break
+
+    # if platform not found, return error
+    if not pf_check or not platform:
+        return jsonify({
+            "error": "Invalid platform",
+            "code": 400,
+            "message": f"Platform not found, please specify platform by adding `platform` parameter, or check if `{platform}` is a valid platform"
+        }), 400
+
+    # if platform ID not found, return error
+    if not platform_id:
+        return jsonify({
+            "error": "Invalid platform ID",
+            "code": 400,
+            "message": "Platform ID not found, please specify platform ID by adding `platform_id` parameter"
+        }), 400
+
+    # if target specified but not found, return error
+    if not tg_check and target:
+        return jsonify({
+            "error": "Invalid target",
+            "code": 400,
+            "message": f"Target {target} not found"
+        }), 400
+
+    # if target not specified, redirect to platform ID
+    if not target:
+        uri = route_path[platform] + str(platform_id)
+        if not israw:
+            return redirect(uri)
+        else:
+            return Response(uri, mimetype="text/plain", status=200)
+
+    # if target specified, redirect to target
+    maps = platform_id_content(platform=platform, platform_id=platform_id)
+    uri = ""
+    if target not in ["trakt", "simkl"]:
+        uri = f"{route_path[target]}{maps[target]}"
+    if target == "trakt":
+        media_type = maps["trakt_type"]
+        season = maps["trakt_season"]
+        if not season:
+            uri = f"{route_path[target]}{media_type}/{maps[target]}"
+        else:
+            uri = f"{route_path[target]}{media_type}/{maps[target]}/seasons/{season}"
+    if target == "simkl":
+        # if does not have anidb id, raise error
+        if not maps["anidb"]:
+            return jsonify({
+                "error": "Not found",
+                "code": 404,
+                "message": "AniDB ID not found, which is the main database source for Simkl. Please issue missing show to SIMKL or create a creq on AniDB if the entry is not a special or OVA"
+            }), 404
+        uri = f"https://api.simkl.com/redirect?to=Simkl&anidb={maps['anidb']}"
+    else:
+        try:
+            title = maps["title"]
+        except KeyError:
+            title = "(Unknown title)"
+        return jsonify({
+            "error": "Not found",
+            "code": 404,
+            "message": f"{title} does not exist on {target} using {platform} with ID {platform_id}"
+        }), 404
+
+    if not israw:
+        return redirect(uri)
+    else:
+        return Response(uri, mimetype="text/plain", status=200)
 
 # general error handler
+
+
 @app.errorhandler(Exception)
 def handle_error(err: Exception):
     """
