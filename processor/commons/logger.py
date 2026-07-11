1c185e1352d33d88b11fbce8f3d270116fd04ae0
Natsu Tadama <nattadasu@nattadasu.my.id>
2026-07-12 03:59:05 +0700

    fix(log): do not show path

--- processor/commons/logger.py
+++ processor/commons/logger.py
@@ -1,9 +1,17 @@
 import logging
+
 from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True, highlighter=None, rich_tracebacks=True)]
+    level="INFO",
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(
+            markup=True, highlighter=None, rich_tracebacks=True, show_path=False
+        )
+    ],
 )
 
 log = logging.getLogger("rich")
